2dd905abdf4eb5eb7b8515ce43e135ba89b61916
Asheesh Laroia <asheesh@asheesh.org>
2016-07-25 15:56:38 -0700

    Better formatting

--- do-it.sh
+++ do-it.sh
@@ -54,7 +54,6 @@ SUCCESSFULLY_BOUND_TO_PORT="no"
 echo -n "Waiting for Sandstorm install to complete... (up to 120 seconds)"
 for i in $(seq 0 120) ; do
   if nc -z -w 1 "$IP_ADDRESS" 80 ; then
-    echo "Server online! Access it at: http://${DESIRED_SANDCATS_NAME}.sandcats-dev.sandstorm.io/setup/token/${ADMIN_TOKEN}"
     SUCCESSFULLY_BOUND_TO_PORT="yes"
     break
   else
@@ -62,8 +61,11 @@ for i in $(seq 0 120) ; do
     sleep 1
   fi
 done
+
 echo ''
 
 if [ "$SUCCESSFULLY_BOUND_TO_PORT" = "no" ] ; then
   echo "*** WARNING: Your server failed to start properly. Contact support."
+else
+  echo "Server online! Access it at: http://${DESIRED_SANDCATS_NAME}.sandcats-dev.sandstorm.io/setup/token/${ADMIN_TOKEN}"
 fi
